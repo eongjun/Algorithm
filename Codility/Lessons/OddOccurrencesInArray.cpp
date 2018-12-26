0dd3c1b0d1386ccd16ec1438dd5334df488438ce
eongjun <39583335+eongjun@users.noreply.github.com>
2018-12-26 20:22:05 +0900

    Update OddOccurrencesInArray.cpp

--- Codility/Lessons/OddOccurrencesInArray.cpp
+++ Codility/Lessons/OddOccurrencesInArray.cpp
@@ -1,3 +1,37 @@
+/*using set*/
+// you can use includes, for example:
+// #include <algorithm>
+#include <vector>
+#include <set>
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(vector<int> &A) {
+	// write your code in C++14 (g++ 6.2.0)
+	set<int> B;
+	std::set<int>::iterator it;
+
+
+	if (A.size()>1) {
+		for (std::vector<int>::size_type i = 0; i<A.size(); i++) {
+			it = B.find(A[i]);
+			if (it != B.end()) {
+				B.erase(it);
+			}
+			else {
+				B.insert(A[i]);
+			}
+		}
+		it = B.begin();
+		return *it;
+	}
+	else {
+		return A[0];
+	}
+
+}
+
+/*using XOR*/
 // you can use includes, for example:
 // #include <algorithm>
 #include <vector>
@@ -6,28 +40,10 @@
 
 int solution(vector<int> &A) {
 	// write your code in C++14 (g++ 6.2.0)
-	vector<int> B;
-	int cnt = 0;
-
-	while (true) {
-		B.clear();
-		cnt = 0;
-		if (A.size()>1) {
-			for (std::vector<int>::size_type i = 1; i<A.size(); i++) {
-				if (A[i] == A[0]) {
-					cnt++;
-				}
-				else {
-					B.push_back(A[i]);
-				}
-			}
-			if ((cnt % 2) == 0) {
-				return A[0];
-			}
-			A = B;
-		}
-		else {
-			return A[0];
-		}
+    int ans=0;
+	for (std::vector<int>::size_type i = 0; i<A.size(); i++) {
+	    ans ^= A[i];
 	}
+		
+    return ans;
 }
